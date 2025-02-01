1d2fc6f00f8dc1b7bb183cc32a7e6ceaab62ddf0
Petar Trajanoski <66224481+HyRALis@users.noreply.github.com>
2025-02-01 19:59:24 +0100

    MAIN: Working on the theme (adding the colors)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,22 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    content: [
+        './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/app/**/*.{js,ts,jsx,tsx,mdx}'
+    ],
+    theme: {
+        extend: {
+            colors: {
+                background: 'var(--background)',
+                foreground: 'var(--foreground)',
+                primary: 'var(--primary)',
+                secondary: 'var(--secondary)',
+                danger: 'var(--danger)',
+                darkBlue: 'var(--darkBlue)'
+            }
+        }
     },
-  },
-  plugins: [],
+    plugins: []
 } satisfies Config;
