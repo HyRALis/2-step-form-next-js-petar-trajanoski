7e56dd4363b3d03e0d180183e72e09e7be326ad0
Petar Trajanoski <66224481+HyRALis@users.noreply.github.com>
2025-02-02 23:13:50 +0100

    MAIN: Created the SearchBar and FormDropdown components

--- src/components/organisms/FormTab2.tsx
+++ src/components/organisms/FormTab2.tsx
@@ -1,16 +1,31 @@
 import React from 'react';
+
 import { AdditionalInfo } from './AdditionalInfo';
-import { Input } from '../ui/molecules/Input';
 import { Heading } from '../ui/atoms/Heading';
+import { Button } from '../ui/atoms/Button';
+import { FormLabel } from '../ui/atoms/form/FormLabel';
+import { FormInput } from '../ui/atoms/form/FormInput';
+import { FormErrorMessage } from '../ui/atoms/form/FormErrorMessage';
+import { FormDropdown } from '../ui/molecules/FormDropdown';
 
 export const FormTab2 = () => {
     return (
         <>
-            <Heading text="Let's validate your number" />
-            <div className="flex space-y-1 w-full">
-                <Input label="Phone number" errorMessage="This field is required" />
+            <div className="flex flex-col space-y-4 mb-6">
+                <Heading text="Let's validate your number" />
+                <div className="flex flex-col space-y-1 w-full">
+                    <FormLabel text="Phone number" />
+                    <div className="flex space-x-1 w-full">
+                        <FormDropdown value="+44" />
+                        <FormInput placeholder="07890 123456" className="placeholder:text-light" />
+                    </div>
+                    <FormErrorMessage errorMessage="This field is required" />
+                </div>
             </div>
-            <AdditionalInfo />
+            <div className="flex flex-col">
+                <AdditionalInfo />
+                <Button variant="primary" className="w-full" size="md" text="Continue" />
+            </div>
         </>
     );
 };
---
+++ src/components/ui/atoms/SearchBar.tsx
@@ -0,0 +1,61 @@
+'use client';
+
+import React from 'react';
+
+import { SearchIcon } from '@/assets/icons/SearchIcon';
+import { CircleTimesIcon } from '@/assets/icons/CircleTimesIcon';
+import { Button } from './Button';
+
+export interface SearchBarProps {
+    getSearchResults: (query: string) => void;
+}
+
+export const SearchBar: React.FC<SearchBarProps> = ({ getSearchResults }) => {
+    const containerRef = React.useRef<HTMLDivElement>(null);
+
+    const [searchQuery, setSearchQuery] = React.useState('');
+
+    React.useEffect(() => {
+        getSearchResults(searchQuery);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [searchQuery]);
+
+    const onInputFocus = React.useCallback(() => {
+        if (containerRef.current) {
+            containerRef.current.classList.add('border-primary');
+        }
+    }, [containerRef.current]);
+
+    const onInputFocusOut = React.useCallback(() => {
+        if (containerRef.current) {
+            containerRef.current.classList.remove('border-primary');
+        }
+    }, [containerRef.current]);
+
+    return (
+        <div
+            ref={containerRef}
+            className="flex items-center justify-between py-4 px-6 w-full rounded-full border-2 border-darkBlue12 focus:border-primary bg-transparent transition-all duration-200 ease-in-out"
+        >
+            <input
+                value={searchQuery}
+                placeholder="Search"
+                className="placeholder:text-light bg-transparent focus:border-none outline-none"
+                onFocus={onInputFocus}
+                onBlur={onInputFocusOut}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => setSearchQuery(e.target.value)}
+            />
+            {searchQuery ? (
+                <Button
+                    className="p-0 hover:text-primary"
+                    variant="icon"
+                    size="xs"
+                    icon={<CircleTimesIcon />}
+                    onClick={() => setSearchQuery('')}
+                />
+            ) : (
+                <SearchIcon />
+            )}
+        </div>
+    );
+};
