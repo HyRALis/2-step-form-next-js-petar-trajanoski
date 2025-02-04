882b8a962324fcac1b3f108c3eb41de5df75996d
Petar Trajanoski <66224481+HyRALis@users.noreply.github.com>
2025-02-04 22:39:05 +0100

    MAIN: Added Vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { EB_Garamond, Hanken_Grotesk } from 'next/font/google';
 import './globals.css';
 import { tailwindMerge } from '@/utils/tailwindMerge';
 import { UserProvider } from '@/context/MainContext';
+import { Analytics } from '@vercel/analytics/next';
 
 const ebGaramond = EB_Garamond({
     variable: '--font-eb-garamond',
@@ -27,6 +28,7 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body className={tailwindMerge([`${ebGaramond.variable}`, `${hkGrotesk.variable}`, 'antialiased'])}>
+                <Analytics />
                 <UserProvider>{children}</UserProvider>
             </body>
         </html>
