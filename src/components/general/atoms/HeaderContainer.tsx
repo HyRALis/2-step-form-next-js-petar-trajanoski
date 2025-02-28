09f42f28913ab0ecd99ba7725f4fa1e0a5faa6f7
Petar Trajanoski <66224481+HyRALis@users.noreply.github.com>
2025-02-28 02:21:53 +0100

    EXPERIMENTAL: Fix for html semantic element

--- src/components/general/atoms/HeaderContainer.tsx
+++ src/components/general/atoms/HeaderContainer.tsx
@@ -9,13 +9,13 @@ export interface HeaderContainerProps {
 
 export const HeaderContainer: React.FC<HeaderContainerProps> = ({ children, className }) => {
   return (
-    <div
+    <header
       className={tailwindMerge([
         className,
         'flex justify-between items-center fixed top-0 z-10 w-full h-[72px] backdrop-blur-xl px-2',
       ])}
     >
       {children}
-    </div>
+    </header>
   );
 };
