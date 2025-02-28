3d7ddbba86827e824d130b945b5ce8941e5a39c8
Petar Trajanoski <66224481+HyRALis@users.noreply.github.com>
2025-02-28 10:03:04 +0100

    EXPERIMENTAL: Fix for remote images

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    /* config options here */
-    images: {
-        domains: ['flagcdn.com', 'upload.wikimedia.org']
-    }
+  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'flagcdn.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'upload.wikimedia.org',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
